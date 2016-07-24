4fda3487f02f28d91a09c1b5bf537f44d468ca6c
hideo54 <hideo54@hideo54.com>
2016-07-24 14:18:08 +0900

    Add WKWebView and url-based search feature.

--- iOS-WebView/ViewController.swift
+++ iOS-WebView/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    let webView = WKWebView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        searchBar.delegate = self
+        
+        self.view.addSubview(webView)
+        
+        let views = ["webView": webView]
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activateConstraints(
+            NSLayoutConstraint.constraintsWithVisualFormat("V:|-108-[webView]-0-|", options: .AlignAllTop, metrics: nil, views: views)
+        )
+        NSLayoutConstraint.activateConstraints(
+            NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[webView]-0-|", options: .AlignAllTop, metrics: nil, views: views)
+        )
+
+        let url:NSURL = NSURL(string: "https://www.google.co.jp/")!
+        webView.loadRequest(NSURLRequest(URL: url))
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        self.view.endEditing(true)
+        guard let url = NSURL(string: searchBar.text!) else {
+            // Error handling
+            return
+        }
+        webView.loadRequest(NSURLRequest(URL: url))
+    }
 
 }
 
